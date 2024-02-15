1c1996ab4f1b2d4e81bd5efe06cf61a546713716
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-15 16:40:50 -0700

    Add right-movement functionality to player ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,6 +22,7 @@ class AlienInvasion:
         """"Start the main loop for the gamae"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()    
     
     def _check_events(self):
@@ -29,6 +30,13 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Update the images on the screen then flip to the new screen"""
