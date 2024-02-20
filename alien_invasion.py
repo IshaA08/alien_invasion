05ec69d7feda694bcd2de1bac176bc5eea6a2d03
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-20 12:03:03 -0700

    Refactor main game loop by adding method for updating bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,14 +26,9 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
 
-            # Get rid of bullets that are off-screen
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-    
     def _check_events(self):
         """Respond to key presses and mouse events"""
         for event in pygame.event.get():
@@ -67,6 +62,16 @@ class AlienInvasion:
         if (len(self.bullets) < self.settings.bullets_allowed):
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update the position of bullets and get rid of old bullets"""
+        # Update bullet positions
+        self.bullets.update()
+
+        # Get rid of bullets that are off-screen
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update the images on the screen then flip to the new screen"""
