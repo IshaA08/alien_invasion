bd00b851fd80c6d091ed6ad391135a8e65849202
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-22 10:43:58 -0700

    Make the bullet go a little faster

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ class Settings:
         self.fleet_direction = 1
 
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.6
         self.bullets_allowed = 3
         self.bullet_width = 1000
         self.bullet_height = 15
