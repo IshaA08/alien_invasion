5113515cfd3b286c332a373f09981af602a42501
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-22 17:20:08 -0700

    Make play button appear at the beginning of the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from ship import Ship
 from bullet import Bullet
 from alien import Alien
 from game_stats import GameStats
+from button import Button
 
 class AlienInvasion:
     """"Overall class to manage game assets and behavior."""
@@ -26,6 +27,7 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
         """"Start the main loop for the gamae"""
@@ -186,6 +188,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the play button if the game is inactive
+        if not self.stats.game_active:
+            self.play_button.draw_button()
         
         pygame.display.flip()
 
--- game_stats.py
+++ game_stats.py
@@ -7,6 +7,9 @@ class GameStats:
         self.reset_stats()
         self.game_active = True
 
+        # Start the game in an inactive state
+        self.game_active = False
+
     def reset_stats(self):
         """Initizalize statistics that can change during the game"""
         self.ships_left = self.settings.ship_limit
